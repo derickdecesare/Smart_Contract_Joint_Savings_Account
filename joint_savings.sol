f335a08fcf74bf41021986f4fd323408032d9974
derickdecesare <deerriicckk@gmail.com>
2022-10-30 17:47:46 -0700

    updated deposit function and fallback function

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,20 +1,5 @@
-/*
-Joint Savings Account
+//Joint Savings Account
 ---------------------
-
-To automate the creation of joint savings accounts, you will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. Your smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
-
-The Starting file provided for this challenge contains a `pragma` for solidity version `5.0.0`.
-You will do the following:
-
-1. Create and work within a local blockchain development environment using the JavaScript VM provided by the Remix IDE.
-
-2. Script and deploy a **JointSavings** smart contract.
-
-3. Interact with your deployed smart contract to transfer and withdraw funds.
-
-*/
-
 pragma solidity ^0.5.0;
 
 // Define a new contract named `JointSavings`
@@ -67,7 +52,7 @@ contract JointSavings {
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance 
         //of the contract.
-        contractBalance = address(this).balance
+        contractBalance = address(this).balance;
     }
 
     // Define a `public payable` function named `deposit`.
@@ -76,7 +61,7 @@ contract JointSavings {
         /*
         Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance`.
         */
-        // YOUR CODE HERE!
+        contractBalance = address(this).balance;
     }
 
     /*
@@ -85,11 +70,13 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        // YOUR CODE HERE!
+        accountOne = account1;
+        accountTwo = account2;
+    
     }
 
     /*
-    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
+    Add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
     */
-    // YOUR CODE HERE!
+    function() external payable {}
 }
